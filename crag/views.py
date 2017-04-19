e3e278922979b4701cd62ec5daa79c78feb01f7a
Kenneth <matovken@gmial.com>
2017-04-20 00:00:07 +0300

    add language support

--- crag/views.py
+++ crag/views.py
@@ -1,5 +1,5 @@
+import base64
 import json
-import urllib
 from google.appengine.api import urlfetch
 import webapp2
 
@@ -13,10 +13,13 @@ class MakeCallRequestHandler(webapp2.RequestHandler):
         VOICE_URL = "http://voice.tmcg.co.ug/hiwa-calls/index.php?r=tocall/create"
         phone = self.request.get('phone').replace(" ", "")
         code = "mcrag/%s" % self.request.get('code')
+        language = self.request.get('language').upper()
+        code = code.replace(".ENG", ".%s" % language)
         post_data = "Tocall[number]=%s&Tocall[class]=%s" % (phone, code)
         result = urlfetch.fetch(url=VOICE_URL, payload=post_data, method=urlfetch.POST,
-                                headers={'Content-Type': 'application/x-www-form-urlencoded'})
+                                headers={'Content-Type': 'application/x-www-form-urlencoded',
+                                         "Authorization": "Basic %s" % base64.b64encode("tmcg:tmcg4321")})
         if 199 < result.status_code < 300:
             self.response.write(json.dumps({"success": True}))
         else:
-            self.response.write(json.dumps({"success": False, "code": result.status_code}))
+            self.response.write(json.dumps({"success": False, "code": result.status_code, 'content': result.content}))
